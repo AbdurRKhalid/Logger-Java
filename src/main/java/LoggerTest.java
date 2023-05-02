e475059aa4571a57aa5f5f0bf128559fd09981f8
ABDUR REHMAN KHALID <abdur.r.khalid@gmail.com>
2023-05-02 10:34:23 +0200

    Added the Comments for Working

--- src/main/java/LoggerTest.java
+++ src/main/java/LoggerTest.java
@@ -1,7 +1,9 @@
 public class LoggerTest {
     public static void main(String[] args) {
         Logger logger = new Logger(LogLevel.WARNING);
-        logger.log("This is a warning message", LogLevel.WARNING);
+        // This will not work.
+        logger.log("This is a warning message", LogLevel.DEBUG);
+        // This will work.
         logger.log("This is an error message", LogLevel.ERROR);
     }
 }
